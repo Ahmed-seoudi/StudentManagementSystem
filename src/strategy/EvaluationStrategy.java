15a56b2a5f666cd8a9759905ea71e241112e7bbb
Ahmed-seoudi <161844776+Ahmed-seoudi@users.noreply.github.com>
2024-12-25 05:14:23 +0200

    Delete src directory

--- src/strategy/EvaluationStrategy.java
+++
@@ -1,7 +0,0 @@
-package strategy;
-
-public interface EvaluationStrategy {
-    
-    // Method to evaluate the grades (to be implemented by concrete strategies)
-    double evaluate(double[] grades);
-}
